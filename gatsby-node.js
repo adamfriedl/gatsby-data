7aee2aa0f89c917fb046bf9b3691b9ed6c1d00f3
Adam Friedl <adam.friedl@rakuten.com>
2019-03-21 07:38:06 -0400

    first commit

---
+++ gatsby-node.js
@@ -0,0 +1,6 @@
+exports.onCreateNode = ({ node }) => {
+  if (node.internal.type === `MarkdownRemark`) {
+    const fileNode = getNode(node.parent)
+    console.log(`\n`, fileNode.relativePath)
+  }
+}
